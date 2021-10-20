43373610fbb44c120a837869141b32a653e675db
hitaloalvess <hitalo.ralves@outlook.com>
2021-10-20 14:59:33 -0300

    terafa: Adicionar funcao fetchImages, useInfiniteQuery e formattedData a pagina index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Button, Box } from '@chakra-ui/react';
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useInfiniteQuery } from 'react-query';
 
 import { Header } from '../components/Header';
@@ -9,6 +9,10 @@ import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
 export default function Home(): JSX.Element {
+
+
+  const fetchImages = async ({pageParam = 0}) => await api.get(`/api/images?after=${pageParam}`);
+
   const {
     data,
     isLoading,
@@ -18,13 +22,14 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+    fetchImages,
+    {
+      getNextPageParam: (lastPage, pages) => lastPage.data.after
+    }
   );
 
-  const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+  const formattedData = useMemo(() =>{
+    return data?.pages.map( response => response.data).flatMap(page => page.data);
   }, [data]);
 
   // TODO RENDER LOADING SCREEN
@@ -35,10 +40,10 @@ export default function Home(): JSX.Element {
     <>
       <Header />
 
-      <Box maxW={1120} px={20} mx="auto" my={20}>
-        <CardList cards={formattedData} />
+      {/* <Box maxW={1120} px={20} mx="auto" my={20}> */}
+        {/* <CardList cards={formattedData} /> */}
         {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
-      </Box>
+      {/* </Box> */}
     </>
   );
 }
